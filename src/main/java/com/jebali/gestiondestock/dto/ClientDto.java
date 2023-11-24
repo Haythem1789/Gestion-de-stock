65370235686c61514373f527526db51d47e0a230
Haythem1789 <jablouch1789@gmail.com>
2023-11-24 11:06:28 +0100

    dto client

--- src/main/java/com/jebali/gestiondestock/dto/ClientDto.java
+++ src/main/java/com/jebali/gestiondestock/dto/ClientDto.java
@@ -2,6 +2,7 @@ package com.jebali.gestiondestock.dto;
 
 import com.jebali.gestiondestock.model.Adresse;
 
+import com.jebali.gestiondestock.model.Client;
 import com.jebali.gestiondestock.model.CommandeClient;
 import lombok.Builder;
 import lombok.Data;
@@ -33,4 +34,32 @@ public class ClientDto {
     private String numtel;
 
     private List<CommandeClientDto> commandeclient;
+
+    public ClientDto fromEntity(Client client){
+        if(client == null){
+            return null;
+        }
+        return ClientDto.builder()
+                .id(client.getId())
+                .nom(client.getNom())
+                .prenom(client.getPrenom())
+                .photo(client.getPhoto())
+                .numtel(client.getNumtel())
+                .build();
+
+
+    }
+
+    public Client toEntity(ClientDto clientDto){
+        if(clientDto == null){
+            return null;
+        }
+        Client client = new Client();
+        client.setId(client.getId());
+        client.setNom(client.getNom());
+        client.setPrenom(client.getPrenom());
+        client.setPhoto(client.getPhoto());
+        client.setNumtel(client.getNumtel());
+        return  client;
+    }
 }
