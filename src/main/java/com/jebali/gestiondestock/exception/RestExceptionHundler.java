14563bc6a2f1f0c9599f2a50a1ace3259a0677cf
Haythem1789 <jablouch1789@gmail.com>
2023-11-29 18:12:47 +0100

    errordto

---
+++ src/main/java/com/jebali/gestiondestock/exception/RestExceptionHundler.java
@@ -0,0 +1,34 @@
+package com.jebali.gestiondestock.exception;
+
+import com.jebali.gestiondestock.Hundlers.ErrorDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class RestExceptionHundler extends ResponseEntityExceptionHandler {
+@ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleException(EntityNotFoundException exception , WebRequest webRequest){
+    final HttpStatus notFound = HttpStatus.NOT_FOUND;
+    final      ErrorDto errorDto =new ErrorDto().builder()
+            .codes(exception.getErrorCodes())
+            .httpcode(notFound.value())
+            .message(exception.getMessage())
+            .build();
+    return new  ResponseEntity<>(errorDto,notFound);
+}
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleException(invalidEntityException exception , WebRequest webRequest){
+        final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        final ErrorDto errorDto =new ErrorDto().builder()
+                .codes(exception.getErrorCodes())
+                .httpcode(badRequest.value())
+                .message(exception.getMessage())
+                .build();
+        return new  ResponseEntity<>(errorDto,badRequest);
+    }
+}
--- src/main/java/com/jebali/gestiondestock/repository/ArticleRepository.java
+++ src/main/java/com/jebali/gestiondestock/repository/ArticleRepository.java
@@ -2,6 +2,9 @@ package com.jebali.gestiondestock.repository;
 
 import com.jebali.gestiondestock.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Integer, Article> {
 
